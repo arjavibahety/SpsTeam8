0822067240eb6b1ea0ca4f6327973943b11a37ec
Bao Xian TSANG <bxtsang.2018@sis.smu.edu.sg>
2020-07-07 14:50:04 +0800

    Create doGet method for retrieving all rooms

--- webproject/src/main/java/servlets/RoomServlet.java
+++ webproject/src/main/java/servlets/RoomServlet.java
@@ -7,7 +7,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.gson.Gson;
 
+
+@WebServlet("/room")
 public class RoomServlet extends HttpServlet {
-  
+  private static DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+  private static Gson gson = new Gson();
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Query query = new Query("Room");
+    PreparedQuery results = datastore.prepare(query);
+    List<Entity> rooms =  results.asList(FetchOptions.Builder.withDefaults());
+
+    response.setContentType("text/json");
+    String jsonResponse = gson.toJson(rooms);
+    response.getWriter().println(jsonResponse);
+  }
 }
