9832955af34382d44b48bcfe2f46ca02f3caec3c
Bao Xian TSANG <bxtsang.2018@sis.smu.edu.sg>
2020-07-07 14:30:17 +0800

    Create Room Servlet

---
+++ webproject/src/main/java/servlets/RoomServlet.java
@@ -0,0 +1,13 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RoomServlet extends HttpServlet {
+  
+}
