68686e7fe7ff00d62a2a69d08014b787cc6458ff
arjavibahety <arjavibahety@gmail.com>
2020-07-30 00:02:15 +0800

    WIP

--- webproject/src/main/java/com/google/sps/servlets/GuiceConfig.java
+++ webproject/src/main/java/com/google/sps/servlets/GuiceConfig.java
@@ -7,10 +7,12 @@ import com.google.inject.servlet.ServletModule;
 import com.google.sps.services.implementations.BlobstoreServiceImpl;
 import com.google.sps.services.implementations.CloseRoomServiceImpl;
 import com.google.sps.services.implementations.EntryServiceImpl;
+import com.google.sps.services.implementations.JoinServiceImpl;
 import com.google.sps.services.implementations.TextMessageServiceImpl;
 import com.google.sps.services.interfaces.BlobstoreService;
 import com.google.sps.services.interfaces.CloseRoomService;
 import com.google.sps.services.interfaces.EntryService;
+import com.google.sps.services.interfaces.JoinService;
 import com.google.sps.services.interfaces.LandingService;
 import com.google.sps.services.implementations.LandingServiceImpl;
 import com.google.sps.services.interfaces.TextMessageService;
@@ -27,6 +29,7 @@ public class GuiceConfig extends GuiceServletContextListener {
         bind(TextMessageService.class).to(TextMessageServiceImpl.class);
         bind(EntryService.class).to(EntryServiceImpl.class);
         bind(CloseRoomService.class).to(CloseRoomServiceImpl.class);
+        bind(JoinService.class).to(JoinServiceImpl.class);
 
         serve("/landing").with(LandingServlet.class);
         serve("/blobstore").with(BlobstoreServlet.class);
